bbb9b68a54eed7f1e1551d763670599d28f36c75
srirajamanikandan <srirajamanikandan310@gmail.com>
2022-07-15 19:39:04 +0530

    Greens

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,57 @@
+package org.add;
+
+public class GreensTech {
+
+	private void greensOmr() {
+		// TODO Auto-generated method stub
+System.out.println("Green OMR = 01");
+	}
+	private void greensAdyar() {
+		// TODO Auto-generated method stub
+System.out.println("Green Adyar = 02");
+	}
+	
+	private void greensTambaram() {
+		// TODO Auto-generated method stub
+System.out.println("Green Tambaram = 03");
+	}
+	
+	private void greensVelachurry() {
+		// TODO Auto-generated method stub
+System.out.println("Green Velachurry = 04");
+	}
+	
+	private void greensAnnaNagar() {
+		// TODO Auto-generated method stub
+System.out.println("Green AnnaNagar = 05");
+
+	}
+	public static void main(String[] args) {
+		 GreensTech s = new GreensTech();
+		 s.greensOmr();
+		 s.greensAdyar();
+		 s.greensTambaram();
+		 s.greensVelachurry();
+		 s.greensAnnaNagar();
+		
+		
+		
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
